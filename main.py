d43caebad912beef361db866adba409d9ddb451a
adrie <1750927@uab.cat>
2025-04-25 13:20:05 +0200

    Renamed stuff

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-import numpy as np
-import typing
 
-class Ratings:
-    
-    def __init__(self, users):
-        self._users = users
-        
-    
-    def simple_recommendation(self):
-        pass
-    
-    def user_based_recommendation(self):
-        pass
-    
-    def content_based_recommendation(self):
-        pass
-    
+def load_classes():
+    pass
---
+++ rating_sistem.py
@@ -0,0 +1,18 @@
+import numpy as np
+import typing
+# from user import User
+
+class Ratings:
+    
+    def __init__(self, users):
+        self._users = users
+        # self._ratings_table = "a"
+    
+    def simple_recommendation(self):
+        pass
+    
+    def user_based_recommendation(self):
+        pass
+    
+    def content_based_recommendation(self):
+        pass
