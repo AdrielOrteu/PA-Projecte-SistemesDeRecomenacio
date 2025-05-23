9c2919003d6cc641467493e780c8fa0d947840bf
adrie <1750927@uab.cat>
2025-05-23 13:56:18 +0200

    Improvements in CONTENTS structure :)

--- expected_version/Contents.py
+++ expected_version/Contents.py
@@ -5,18 +5,46 @@ from abc import ABC, abstractmethod
 import typing
 from typing import List, TypeVar, Any, NoReturn
 
-class Contents(ABC):
-    def __init__(self) -> None:
-        self._contents: dict[str, tuple[str, dict[str, Any]]] | None = None # { "id":("title", {characteristics}), ...}
+
+class Content:
+    
+    def __init__(self, identifier: int, title: str, **characteristics) -> None:
+        self._identifier : int = identifier
+        self._title : str = title
+        self._characteristics : dict[str, Any] = characteristics
     
     @property
-    def contents(self) -> list[tuple[str, str, dict[str, Any]]]:
+    def identifier(self) -> int:
+        return self._identifier
+    @identifier.setter
+    def identifier(self, new_identifier: int) -> None:
+        self._identifier = new_identifier
+    
+    @property
+    def title(self) -> str:
+        return self._title
+    @title.setter
+    def title(self, new_title: str) -> None:
+        self._title = new_title
+    
+    @property
+    def characteristics(self) -> dict[str, Any]:
+        return self._characteristics
+    
+
+
+class Contents(ABC):
+    def __init__(self) -> None:
+        self._contents: dict[int, Content] | None = None # { "id":("title", {characteristics}), ...}
+    
+    @property
+    def contents(self) -> dict[int, Content]:
         if self._contents is None:
             raise ValueError("contents are not loaded")
         return self._contents
     
     @property
-    def identifiers(self) -> NDArray[str]:
+    def identifiers(self) -> NDArray[int]:
         if self._contents is None:
             raise ValueError("contents are not loaded")
         return np.array([content[0] for content in self._contents])
@@ -55,6 +83,9 @@ class Books(Contents):
                                    {"BookAuthor":book.BookAuthor,
                                     "YearOfPublication":book.YearOfPublication,
                                     "Publisher":book.Publisher}))
+    
+    def save_contents(self):
+        pass #TODO
 
 
 class Movies(Contents):
@@ -63,3 +94,6 @@ class Movies(Contents):
         movies_df = pd.read_csv("movies/movies.csv")
         for movie in movies_df.iterables(index=False, name='Pandas'):
             self._contents.append((movie.movieId, movie.title, {"genres":movie.genres}))
+    
+    def save_contents(self):
+        pass #TODO
