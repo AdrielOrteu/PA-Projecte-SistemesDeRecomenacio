489635bc60e0cabf55370efb57627869e6f4387a
Maxirenia <94077336+Maxirenia@users.noreply.github.com>
2025-05-25 21:36:19 +0200

    Add files via upload

--- expected_version/Evaluate.py
+++ expected_version/Evaluate.py
@@ -1,6 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 25 14:44:28 2025
 
-def mean_absolute_error():
-    pass
+@author: maxtr
+"""
+
+def mean_absolute_error(user_id):
+    prediction = np.array(predict_rate(user_id))
+    mae = np.abs(Rating.ratings - prediction).mean()
 
 def root_mean_square_error():
-    pass
+    prediction = np.array(predict_rate(user_id))
+    rmse = np.sqrt(((Rating.ratings - prediction)**2).mean())
