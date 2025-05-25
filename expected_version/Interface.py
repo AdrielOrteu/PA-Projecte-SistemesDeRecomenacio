e96bbbce95400d55c78e023ab116ed8db802af47
adrie <1750927@uab.cat>
2025-05-25 08:58:57 +0200

    saving my changes: CHANGES:  - worked on the main (v3)  - worked on the GUI

--- expected_version/Interface.py
+++ expected_version/Interface.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, Any
 from numpy.typing import NDArray
 
 class GUI:
@@ -19,10 +19,15 @@ class GUI:
         choice = {"R":"recommend", "E":"evaluate", "B": "Back", "X": "exit"}.get(input())
         return choice
     
+    def chose_method(self) -> str:
+        print("How do you want us to recommend the content?\n S -> simple | L -> collaborative | C -> content")
+        # choice = {"S": self.chose_simple_params, "L": self.chose_collaborative_params, "C": self.chose_content_params}.get(input())
+        choice = {"S": "simple", "L": "collaborative", "C": "content"}.get(input())
+        return choice
+    
     def chose_simple_params(self):
         pass
-    
-    def chose_method(self) -> Callable[[], NDArray[str]]:
-        print("How do you want us to recommend the content?\n S -> simple | L -> collaborative | C -> content")
-        choice = {"S": self.chose_simple_params, "L": "collaborative", "C": "content"}.get(input())
-        return choice
+    def chose_collaborative_params(self):
+        pass
+    def chose_content_params(self):
+        pass
---
+++ expected_version/main_v3.py
@@ -0,0 +1,13 @@
+from Interface import GUI
+from Contents import Movies
+from Users import MovieUsers
+
+interface = GUI()
+executing = True
+
+interface.chose_db()
+
+task = {}
+
+while executing:
+    pass
